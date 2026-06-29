76db3b9b447da38e037c2e0cfcfb2997abadcc23
Jason Pearson <1414364+kaeawc@users.noreply.github.com>
2026-06-29 09:31:38 -0500

    fix plan yaml schema bundling (#2629)

--- src/utils/plan/planYaml.ts
+++ src/utils/plan/planYaml.ts
@@ -1,5 +1,5 @@
 import * as yaml from "js-yaml";
 
 export const PLAN_YAML_LOAD_OPTIONS: yaml.LoadOptions = {
-  schema: yaml.CORE_SCHEMA.withTags(yaml.mergeTag),
+  schema: new yaml.Schema([...yaml.CORE_SCHEMA.tags, yaml.mergeTag]),
 };

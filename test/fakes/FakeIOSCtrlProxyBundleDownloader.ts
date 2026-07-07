828eb6b72d39712d5606725266387465f314abdf
Jason Pearson <1414364+kaeawc@users.noreply.github.com>
2026-07-07 00:11:41 -0500

    fix(test): remove duplicate iOS bundle fake fields (#3368)

--- test/fakes/FakeIOSCtrlProxyBundleDownloader.ts
+++ test/fakes/FakeIOSCtrlProxyBundleDownloader.ts
@@ -2,7 +2,6 @@ import * as fs from "fs/promises";
 import * as path from "path";
 import { resolveRunnerChecksum } from "../../src/constants/release";
 import type { Sha256Source, CtrlProxyIosBundleDownloader } from "../../src/utils/IOSCtrlProxyBundleDownloader";
-import { resolveRunnerChecksum } from "../../src/constants/release";
 
 export class FakeIOSCtrlProxyBundleDownloader implements CtrlProxyIosBundleDownloader {
   public downloadedUrls: string[] = [];
@@ -11,7 +10,6 @@ export class FakeIOSCtrlProxyBundleDownloader implements CtrlProxyIosBundleDownl
   public runnerChecksum: string = resolveRunnerChecksum();
   public checksumSource: Sha256Source = "node";
   public extractedSubdir: string = "";
-  public runnerChecksum: string = resolveRunnerChecksum();
 
   public async download(url: string, destination: string): Promise<void> {
     this.downloadedUrls.push(url);

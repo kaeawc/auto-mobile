0f87e501b585394d1943b11636158896a3c25f56
Jason Pearson <jason.d.pearson@gmail.com>
2026-09-10 10:36:15 -0500

    fix: pass daemon timer to default session repository

--- src/daemon/daemon.ts
+++ src/daemon/daemon.ts
@@ -306,7 +306,10 @@ export class Daemon {
     options: DaemonOptions = {},
     installedAppsRepository?: InstalledAppsStore,
     timer: Timer = defaultTimer,
-    deviceSessionRepository: DeviceSessionRepository = new DeviceSessionRepository(),
+    deviceSessionRepository: DeviceSessionRepository = new DeviceSessionRepository(
+      undefined,
+      timer,
+    ),
     idGenerator: IdGenerator = defaultIdGenerator,
     databaseInitializer: DatabaseInitializer = new DefaultDatabaseInitializer(),
     startupFailureTracker: StartupFailureTracker = new DefaultStartupFailureTracker(),
